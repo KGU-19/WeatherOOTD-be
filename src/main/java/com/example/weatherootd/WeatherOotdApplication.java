e411e1a655a6639613ca908e7ab49509a79f657f
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-02-15 17:09:32 +0900

    [chore] 백엔드 프로젝트 초기화 (#4)

---
+++ src/main/java/com/example/weatherootd/WeatherOotdApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherootd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherOotdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherOotdApplication.class, args);
+	}
+
+}
