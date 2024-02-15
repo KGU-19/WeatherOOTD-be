6d91fa67b02af346007c4116acf08b6234d61c85
LeeShinHaeng <dragon117@kyonggi.ac.kr>
2024-02-15 17:07:31 +0900

    [chore] 백엔드 프로젝트 초기화

---
+++ src/test/java/com/example/weatherootd/WeatherOotdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherOotdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
