b9f474b0b3f4fb7b2fd6168e626ddf9b438189f0
LeeShinHaeng <dragon117@kyonggi.ac.kr>
2024-02-16 17:49:10 +0900

    [backend] 멤버 레포지토리 생성

---
+++ src/main/java/com/example/weatherootd/reposotory/MemberRepository.java
@@ -0,0 +1,48 @@
+package com.example.weatherootd.reposotory;
+
+import com.example.weatherootd.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    private static final ConcurrentHashMap<Long, Member> store = new ConcurrentHashMap<>();
+    private static AtomicLong sequence = new AtomicLong(0);
+
+
+    public Member save(Member member){
+        member.setId(sequence.getAndIncrement());
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id){
+        Member member = store.get(id);
+        if(member != null){
+            return member;
+        }
+        throw new IllegalArgumentException("존재하지 않는 id");
+    }
+
+    public List<Member> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long memberId, Member inputParam){
+        Member findMember = findById(memberId);
+        findMember.setName(inputParam.getName());
+        findMember.setAge(inputParam.getAge());
+        findMember.setSex(inputParam.getSex());
+        findMember.setStyle(inputParam.getStyle());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
