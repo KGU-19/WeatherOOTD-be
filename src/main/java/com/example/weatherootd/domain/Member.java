0cb4ff37cd025d9129937ab6c1b6712dc37397fe
LeeShinHaeng <dragon117@kyonggi.ac.kr>
2024-02-20 19:13:33 +0900

    [backend, fix] 멤버 도메인 수정 id(Long) -> id(String) password(String) 생성

--- src/main/java/com/example/weatherootd/domain/Member.java
+++ src/main/java/com/example/weatherootd/domain/Member.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class Member {
-    private Long id;
+    private String id;
+    private String password;
     private String name;
     private Integer age;
     private int sex;
